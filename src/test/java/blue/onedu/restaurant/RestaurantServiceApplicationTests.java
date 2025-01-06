1af82cda31d5c2505cdf7240185d402d5d8ed592
PaB1010 <mudodd@naver.com>
2025-01-06 19:48:50 +0900

    git 1st upload

---
+++ src/test/java/blue/onedu/restaurant/RestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package blue.onedu.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
